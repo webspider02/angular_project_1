68de3c4db7bfc272152feec0ddfd5676028d4e6a
webspider02 <giorgoszampakikas@gmail.com>
2024-11-12 19:20:27 +0200

    styles

--- src/app/display-data/display-data.component.ts
+++ src/app/display-data/display-data.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import {MatDividerModule} from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
+import { AddPatientComponent } from '../add-patient/add-patient.component';
 import  { patientData }  from '../data';
 import { Patient } from '../models';
 
 @Component({
   selector: 'app-display-data',
   standalone: true,
-  imports: [MatListModule, CommonModule, MatDividerModule],
+  imports: [MatListModule, CommonModule, MatDividerModule, AddPatientComponent],
   templateUrl: './display-data.component.html',
   styleUrls: ['./display-data.component.scss']
 })
