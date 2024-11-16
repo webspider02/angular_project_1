0f06d3871e52b6b91b8008150cb6d3bcdee6dba2
webspider02 <giorgoszampakikas@gmail.com>
2024-11-16 11:38:21 +0200

    routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { DisplayDataComponent } from './display-data/display-data.component';
 import { SearchPatientComponent } from './search-patient/search-patient.component';
 
 export const routes: Routes = [
+    { 
+        path: '', redirectTo: '/display-data', pathMatch: 'full' 
+    },
     {
         path: 'display-data',
         component: DisplayDataComponent
