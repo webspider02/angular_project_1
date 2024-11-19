36fada94bd341dfa2066ef14ba15c6b940021d88
webspider02 <giorgoszampakikas@gmail.com>
2024-11-19 18:33:33 +0200

    side bar and main content structure

--- src/app/patients-layout/patients-layout.component.ts
+++ src/app/patients-layout/patients-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-patients-layout',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatSidenavModule, MatSelectModule, MatFormField, MatInput],
   templateUrl: './patients-layout.component.html',
   styleUrl: './patients-layout.component.scss'
 })
