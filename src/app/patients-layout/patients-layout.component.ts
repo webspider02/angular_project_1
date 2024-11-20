e2f7ace9afc9351a64c9f25cae0470242de6f81d
webspider02 <giorgoszampakikas@gmail.com>
2024-11-20 11:30:49 +0200

    basic sidenav styles

--- src/app/patients-layout/patients-layout.component.ts
+++ src/app/patients-layout/patients-layout.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { patientData } from '../data';
+import { Patient } from '../models';
 
 @Component({
   selector: 'app-patients-layout',
   standalone: true,
-  imports: [MatCardModule, MatSidenavModule, MatSelectModule, MatFormField, MatInput],
+  imports: [CommonModule, MatCardModule, MatSidenavModule, MatSelectModule, MatFormField, MatInput],
   templateUrl: './patients-layout.component.html',
   styleUrl: './patients-layout.component.scss'
 })
 export class PatientsLayoutComponent {
+  patientData: Patient[] = patientData;
 
+  selectedPatient: Patient | null = null
+
+  selectPatient(patient: Patient): void {
+    this.selectedPatient = patient;
+  }
 }
