2b1a207755da14b1b26ca456b7a06f37d5652747
webspider02 <giorgoszampakikas@gmail.com>
2024-12-11 13:43:53 +0200

    layout part 2

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+import { patientData } from '../data'
+import { Patient } from '../models';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
     MatMenuModule,
     MatSidenavModule,
     MatButtonModule,
@@ -19,5 +30,5 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
+  patients:  Patient[]= patientData;
 }
