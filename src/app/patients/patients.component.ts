35876e1ef6bb64ba6d4e6b298d65d44c4b50520e
webspider02 <giorgoszampakikas@gmail.com>
2024-12-13 16:02:16 +0200

    patients component

--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AddPatientComponent } from '../add-patient/add-patient.component';
+import { DisplayDataComponent } from '../display-data/display-data.component';
+import { patientData } from '../data';
+import { Patient } from '../models';
 
 @Component({
   selector: 'app-patients',
   standalone: true,
   imports: [
+    RouterModule,
+    DisplayDataComponent,
+    AddPatientComponent,
     CommonModule,
   ],
   templateUrl: './patients.component.html',
   styleUrl: './patients.component.scss'
 })
 export class PatientsComponent {
+  patientData: Patient[] = patientData;
 
+  delete(patient: Patient): void {
+    const index = this.patientData.findIndex(p => p.id === patient.id);
+    if (index !== -1) {
+      this.patientData.splice(index, 1);
+    }
+  }
+  
 }
