9c888d6abe9df670a1c23c0de7e36abb93978bda
webspider02 <giorgoszampakikas@gmail.com>
2024-12-13 17:20:43 +0200

    patient-detail component rm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { PatientsComponent } from './patients/patients.component';
 
 export const routes: Routes = [
-
+    { path: '', component: LayoutComponent },
+    { path: 'patients', component: PatientsComponent },
 ];
 
