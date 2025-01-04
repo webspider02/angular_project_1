a4af9a4e2d2eecf79e9ecff1d276ea39af695a39
webspider02 <giorgoszampakikas@gmail.com>
2025-01-04 10:13:56 +0200

    patient list part 2

--- src/app/display-data/display-data.component.ts
+++ src/app/display-data/display-data.component.ts
@@ -3,6 +3,7 @@ import { MatListModule } from '@angular/material/list';
 import {MatDividerModule} from '@angular/material/divider';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule } from '@angular/router';
 import { AddPatientComponent } from '../add-patient/add-patient.component';
 import  { patientData }  from '../data';
 import { Patient } from '../models';
@@ -10,7 +11,15 @@ import { Patient } from '../models';
 @Component({
   selector: 'app-display-data',
   standalone: true,
-  imports: [MatTableModule, MatListModule, CommonModule, MatDividerModule, AddPatientComponent],
+  imports: [
+    RouterModule,
+    RouterLink,
+    MatTableModule,
+    MatListModule,
+    CommonModule,
+    MatDividerModule,
+    AddPatientComponent
+  ],
   templateUrl: './display-data.component.html',
   styleUrls: ['./display-data.component.scss']
 })
